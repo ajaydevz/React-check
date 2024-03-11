bffc4a0248da4431acb8fd37cf0c4c5c7b5acb2f
Ajay dev <ajaydevzzz30@gmail.com>
2024-03-11 19:53:21 +0530

    learn React concept into deep

---
+++ src/components/ComponentE.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ComponentF from './ComponentF'; // Ensure correct relative path
+
+function ComponentE() {
+  return (
+    <div>
+      <ComponentF />
+    </div>
+  );
+}
+
+export default ComponentE;
